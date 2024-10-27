a686bc65030426de1a30ad0b88782f9788d6d659
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-10-27 22:42:12 +0100

    FEAT Assembly Hellow World

---
+++ cleanmem/cleanmem.asm
@@ -0,0 +1,30 @@
+	processor 6502
+
+	seg code
+	org $F000     	; Define the code origin at $F000
+
+Start:
+	sei				; Disable interrupts
+	cld           	; Disable the BCD decimal math mode.
+	ldx #$FF      	; Loads the X register #$FF
+	txs           	; Transfer the X register to the (S)tack Pointer
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;   Clear the Page Zero region of my memory. ($00 - $FF)
+;	Clearing the entire RAM and also the entire TIA registers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	lda #0 			; A = 0
+	ldx #$FF		; X = #$FF
+
+MemLoop:
+	sta $0,X		; Store the value of A (0) to at the memory address in  $0 + X
+	dex				; X--
+	bne	MemLoop		; Go back to MemLoop until Z-flag is set (0)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;   Fill the ROM Size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	org $FFFC		; Set the origin to the last memory address at the rom
+	.word Start		; Reset vector at $FFFC to where the program starts
+	.word Start		; Interrupt vector at $FFFE (Unused in the VCS)
