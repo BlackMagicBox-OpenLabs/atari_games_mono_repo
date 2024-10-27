4ea44000a2c1ed40eb26c56272368c61936e3a72
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-10-28 00:23:17 +0100

    Include headers for Atari with labels for display interactions

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -1,30 +1,31 @@
 	processor 6502
 
-	seg code
-	org $F000     	; Define the code origin at $F000
+	SEG code
+	ORG $F000     	; Define the code origin at $F000
 
 Start:
-	sei				; Disable interrupts
-	cld           	; Disable the BCD decimal math mode.
-	ldx #$FF      	; Loads the X register #$FF
-	txs           	; Transfer the X register to the (S)tack Pointer
+	SEI				; Disable interrupts
+	CLD           	; Disable the BCD decimal math mode.
+	LDA #$FF      	; Loads the X register #$FF
+	TSX           	; Transfer the X register to the (S)tack Pointer
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;   Clear the Page Zero region of my memory. ($00 - $FF)
 ;	Clearing the entire RAM and also the entire TIA registers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	lda #0 			; A = 0
-	ldx #$FF		; X = #$FF
-	sta $FF			; Store A (o) to $FF before start looping to prevent the error by one
-MemLoop:
-	dex				; X--
-	sta $0,X		; Store the value of A (0) to at the memory address in  $0 + X
-	bne	MemLoop		; Go back to MemLoop until Z-flag is set (0)
+	LDA #100		; Load Literal Decimal value 100
+	
+	CLC				; Clear Carry (6507 does not support ADD)
+	ADC #5			; Add 5 to Acc
+	SEC				; Set Carry for Subtraction with Carry
+	SBC #10			; Subtract 10 from Acc
+
+	JMP Start		; To force the infinite loop for debug
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;   Fill the ROM Size to exactly 4KB
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	org $FFFC		; Set the origin to the last memory address at the rom
+	ORG $FFFC		; Set the origin to the last memory address at the rom
 	.word Start		; Reset vector at $FFFC to where the program starts
 	.word Start		; Interrupt vector at $FFFE (Unused in the VCS)
