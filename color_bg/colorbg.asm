0bbae8bdcaea46750a84cf738d50dce6e9f4e4f1
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-10-28 01:04:56 +0100

    Create a make file to build binaries

---
+++ color_bg/colorbg.asm
@@ -0,0 +1,24 @@
+	PROCESSOR	6502
+	
+	INCLUDE	"vcs.h"
+	INCLUDE	"macro.h"
+
+	SEG CODE
+	ORG $F000		; defines the origin of the ROM at $F000
+START:
+	CLEAN_START: 	; Macro to safety clear the memory
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;   Set the background color to Yellow
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	LDA #$1E		; Load Color Yellow NTSC ($1E) https://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Atari_2600
+	STA COLUBK		; Store A to COLUBK ($09)
+
+	JMP START
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;	Fill the ROM Size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	ORG $FFFC		; Set the origin to the last memory address at the rom
+	.word START		; Reset Vector at $FFFC 
+	.word START		; Interrupt Vector at $FFFE (unused in VCS)
+
